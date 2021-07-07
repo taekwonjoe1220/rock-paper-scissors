0d674ce4166b5799bd0ac4d167555abf061c8798
Joe Forster <jforster.advanced@gmail.com>
2021-07-07 14:53:07 -0600

    added UI functionality with buttons. Still need to add score counter and add some reset features

--- script.js
+++ script.js
@@ -12,61 +12,86 @@ function computerPlay() {
 }
 
 // round of play between player & computer
-function playRound(playerSelection, computerSelection) {
+function playRound() {
   // determine winner
+  computerSelection = computerPlay();
+  playerSelection = this.id;
+
+  // draw if both selections are the same
   if (playerSelection === computerSelection) {
-    return outcome[2];
+    displayResults(outcome[2]);
   } else if (
+    // rock beats scissors, paper beats rock, scissors beats paper => player wins
     (playerSelection === selections[0] &&
       computerSelection === selections[2]) ||
     (playerSelection === selections[1] &&
       computerSelection === selections[0]) ||
     (playerSelection === selections[2] && computerSelection === selections[1])
   ) {
-    return outcome[0];
+    displayResults(outcome[0]);
   } else {
-    return outcome[1];
+    displayResults(outcome[1]);
   }
 }
-
-function game() {
-  // initialize game variables
-  playerSelection = "";
-  computerSelection = "";
-  result = "";
-  score = 0;
-  drawCount = 0;
-
-  // series of 5 games
-  for (let i = 0; i < 5; i++) {
-    playerSelection = prompt("Please choose one: rock, paper, or scissors");
-    computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection);
-    result = playRound(playerSelection, computerSelection);
-
-    if (result === "win") {
-      console.log(
-        `You ${result}! ${playerSelection} beats ${computerSelection}`
-      );
-      score++;
-    } else if (result === "lose") {
-      console.log(
-        `You ${result}! ${computerSelection} beats ${playerSelection}`
-      );
-    } else {
-      console.log(`It's a ${result}! You both chose ${playerSelection}`);
-      drawCount++;
-    }
-  }
-  if (score < 3) {
-    console.log(
-      `You lost the best of 5 series ${score} to ${5 - (score + drawCount)}`
-    );
+function displayResults(result) {
+  if (result === "win") {
+    results.textContent = `You ${result}! ${playerSelection} beats ${computerSelection}`;
+    score++;
+  } else if (result === "lose") {
+    results.textContent = `You ${result}! ${computerSelection} beats ${playerSelection}`;
   } else {
-    console.log(
-      `You won the best of 5 series ${score} to ${5 - (score + drawCount)}`
-    );
+    results.textContent = `It's a ${result}! You both chose ${playerSelection}`;
   }
 }
+// function game() {
+//   // initialize game variables
+//   playerSelection = "";
+//   computerSelection = "";
+//   result = "";
+//   score = 0;
+//   drawCount = 0;
 
-game();
+// series of 5 games
+// for (let i = 0; i < 5; i++) {
+//   playerSelection = prompt("Please choose one: rock, paper, or scissors");
+//   computerSelection = computerPlay();
+//   playRound(playerSelection, computerSelection);
+//   result = playRound(playerSelection, computerSelection);
+
+//   if (result === "win") {
+//     console.log(
+//       `You ${result}! ${playerSelection} beats ${computerSelection}`
+//     );
+//     score++;
+//   } else if (result === "lose") {
+//     console.log(
+//       `You ${result}! ${computerSelection} beats ${playerSelection}`
+//     );
+//   } else {
+//     console.log(`It's a ${result}! You both chose ${playerSelection}`);
+//     drawCount++;
+//   }
+// }
+// if (score < 3) {
+//   console.log(
+//     `You lost the best of 5 series ${score} to ${5 - (score + drawCount)}`
+//   );
+// } else {
+//   console.log(
+//     `You won the best of 5 series ${score} to ${5 - (score + drawCount)}`
+//   );
+// }
+// }
+
+// game();
+
+// event listen for button click
+const buttons = document.querySelectorAll("button");
+const container = document.querySelector(".container");
+const results = document.createElement("p");
+container.appendChild(results);
+buttons.forEach((button) => button.addEventListener("click", playRound));
+
+// assign player value to button id (done in playRound function)
+// randomize computer selection (done in playRound function)
+// add text to screen with results (bye bye console.log for now...)
